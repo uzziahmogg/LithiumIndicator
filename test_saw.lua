d7e0c04cb861afc5eac14721d81df6c0a299f82a
Uzziah Mogg <39528065+uzziahmogg@users.noreply.github.com>
2021-11-03 10:45:19 +0300

    Add files via upload

--- test_saw.lua
+++ test_saw.lua
@@ -1,5 +1,5 @@
 Settings = { 
-    Name = "FEK_SawLine" 
+    Name = "FEK_SawLine1" 
 } 
 
 function Init() 
@@ -10,34 +10,43 @@ function Init()
 end 
 
 function OnCalculate(index) 
-    if (index == 1) then
+--[[     if (index == 1) then
         Lines[1] = 1
         Add = -1
     elseif (index == 10) then
         Lines[10] = Add * Lines[1]
-
-    elseif ((index > 7750) and (index < 7770)) then
-        Lines[index] = nil
-
     else
         if (math.fmod(index, Period) == 0) then
-            if Lines[index-Period] == nil then 
-
-                PrintDbgStr("QUIK" .. "|" .. (index-Period) .. "|" .. tostring(Lines[index-Period]) .. "|" .. (index-Period+1) .. "|" .. tostring(Lines[index-Period+1]) .. "|" .. (index-Period+2) .. "|" .. tostring(Lines[index-Period+2]))
-
-                Lines[index-Period] = -3
-                Lines[index-Period+1] = 4
- 
-                PrintDbgStr("QUIK" .. "|" .. (index-Period) .. "|" .. tostring(Lines[index-Period]) .. "|" .. (index-Period+1) .. "|" .. tostring(Lines[index-Period+1]) .. "|" .. (index-Period+2) .. "|" .. tostring(Lines[index-Period+2]))
-            end
             Lines[index] = Lines[index-Period] * Add
         end
     end
+    
 
-    if (index >= 7750) then
-        local t = T(index)
-        PrintDbgStr("QUIK" .. "|" .. t.month .. "|" .. t.day .. "|" .. t.hour .. "|" .. t.min .. "|" .. index .. "|" .. math.fmod(index, Period)  .. "|" .. Add .. "|" .. tostring(Lines[index]))
+    if (index > 7780) then
+        local i
+        for i = 7750, 7760 do
+            Lines[i] = nil    
+        end
+        -- local t = T(index)
+        -- PrintDbgStr("QUIK" .. "|" .. t.month .. "|" .. t.day .. "|" .. t.hour .. "|" .. t.min .. "|" .. index .. "|" .. math.fmod(index, Period)  .. "|" .. Add .. "|" .. tostring(Lines[index]))
     end
 
-    return Lines[index]
+    return Lines[index] ]]
+
+    local ret_value = 0
+	if (index == 1) then
+        PrintDbgStr("Part1:" .. "|" .. index)
+
+		ret_value = 1
+	else
+        PrintDbgStr("Part2:" ..  "|" .. tostring(index) .. "|" .. tostring(GetValue(index-1, 1))  .. "|" ..  tostring(GetValue(index-1, 1)) + 2)
+
+		ret_value = GetValue(index-1, 1) + 2
+	end
+
+	if (index % 10 == 0) then
+        PrintDbgStr("Part3:" .. "|" .. tostring(index) .. "|" .. tostring(SetValue(index-1, 1, 2)) .. "|" .. tostring(index-1) .. ":" .. tostring(GetValue(index-1, 1)))
+	end  
+
+	return ret_value
 end
