883d774540aa8ed93bcb553d0fd1134cb01db90b
Uzziah Mogg <39528065+uzziahmogg@users.noreply.github.com>
2022-02-03 11:48:00 +0300

    Add files via upload

--- test_queue.lua
+++ test_queue.lua
@@ -1,47 +1,70 @@
+-------------------------------------------------------------------------------
+-- Constructor
+-------------------------------------------------------------------------------
+function IndexWindowConstructor(_from, _size)
+	local Windows = { From = _from, Size = _size, Indexes = {}, Values = {} }
 
---todo function to calc number of candles
-function Queue(from, to)
-    local From = from
-    local To = to
-	local Queues = { Indexes = {}, Values = {} }
-    local Size = (to - from + 1)
-
-	return function (index, value)
-		if (index == 1) then
-			Queues = { Indexes = {}, Values = {} }
-		end
-
-		if (--CandleExist(index) and
-        (index >= From) and (index <= To)) then
-            -- insert close price to end of Queues and update Sum of closes
-			table.insert(Queues.Indexes, index)
-            table.insert(Queues.Values, value)
-
-            -- DUBLICATE "and (index <= To)" - if Queues growth up max size
-			--[[ if ((#Queues.Indexes > Size) and (#Queues.Values > Size)) then
-                --remove earlest price from Queues
-				table.remove(Queues.Indexes, 1)
-                table.remove(Queues.Values, 1)
-			end ]]
-		end
-		return Queues
+	return function()
+		return Windows
 	end
 end
 
--- constructor
-q = Queue(11, 33)
-z = Queue(27, 44)
+-------------------------------------------------------------------------------
+-- Add item - index and value to IndexWindow with checking hit inside IndexWindow
+-- _index is global candle index
+-------------------------------------------------------------------------------
+function IndexWindowAddItem(_windows, _index, _value)
+	if (_index == 1) then
+		_windows.Indexes = {}
+		_windows.Values = {}
+	end
 
-print(tostring(q) .. "\t" .. tostring(z))
+	-- append close price to end of IndexWindow
+	if (--[[CandleExist(index) and]] (_index >= _windows.From) and (_index <= (_windows.From + _windows.Size))) then
+		table.insert(_windows.Indexes, _index - _windows.From + 1, _index)
+		table.insert(_windows.Values, _index - _windows.From + 1, _value)
 
-for i = 1, 50 do
-    qq = q(i, i/2)
-	zz = z(i, i*2)
+		-- remove first items of IndexWindow if IndexWindow growth up max Size
+		--[[if ((#_windows.Indexes > _windows.Size) and (#_windows.Values > _windows.Size)) then
+			table.remove(_windows.Indexes, 1)
+			table.remove(_windows.Values, 1)
+		end]]
+
+		return true
+	end
+
+	return false
 end
 
-print(tostring(qq) .. "\t" .. tostring(zz))
+-------------------------------------------------------------------------------
+-- Get item - value by index in window not in array
+-- _index is global candle index
+-------------------------------------------------------------------------------
+function IndexWindowGetItem(_windows, _index)
+	if ((_index >= _windows.From) and (_index <= (_windows.From + _windows.Size))) then
+		return _windows.Values[_index - _windows.From + 1]
+	end
+	return nil
+end
+
+-- constructor
+x = IndexWindowConstructor(11, 33)()
+z = IndexWindowConstructor(27, 44)()
+
+print("---------------")
+print("x: " .. tostring(x) .. "\tz: " .. tostring(z))
+print("x.From: " .. tostring(x.From) .. "\tz.From " .. tostring(z.From))
+print("x.Size: " .. tostring(x.Size) .. "\tz.Size " .. tostring(z.Size))
+print("x.Indexes: " .. tostring(x.Indexes) .. "\tz.Indexes: " .. tostring(z.Indexes))
+print("x.Values: " .. tostring(x.Values) .. "\tz.Values: " .. tostring(z.Values))
+print("---------------")
 
 for i = 1, 50 do
-    print('[' .. i .. ']: ' .. '\tqq[' .. tostring(qq.Indexes[i]).. "]=" .. tostring(qq.Values[i]) .. '\tzz[' .. tostring(zz.Indexes[i]).. "]=" .. tostring(zz.Values[i]))
+    IndexWindowAddItem(x, i, i/2)
+	IndexWindowAddItem(z, i, i*2)
+end
+
+for i = 1, 50 do
+    print('[' .. i .. ']: ' .. '\tx[' .. tostring(x.Indexes[i]).. "]=" .. tostring(x.Values[i]) .. '\tz[' .. tostring(z.Indexes[i]).. "]=" .. tostring(z.Values[i]))
 end
 
