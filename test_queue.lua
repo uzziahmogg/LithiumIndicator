bd7b82148adfc6237074ffc7c1d8c24447c8ad15
Uzziah Mogg <39528065+uzziahmogg@users.noreply.github.com>
2022-02-03 08:34:54 +0300

    Add files via upload

--- test_queue.lua
+++ test_queue.lua
@@ -1,49 +1,47 @@
 
 --todo function to calc number of candles
 function Queue(from, to)
-    From = from
-    To = to
-	Queues = { Indexes = {}, Values = {} }
-    Size = (to - from + 1)
+    local From = from
+    local To = to
+	local Queues = { Indexes = {}, Values = {} }
+    local Size = (to - from + 1)
 
 	return function (index, value)
 		if (index == 1) then
-			Queues = { Indexes = {}, Values = {} }			
+			Queues = { Indexes = {}, Values = {} }
 		end
 
-		if (--CandleExist(index) and 
+		if (--CandleExist(index) and
         (index >= From) and (index <= To)) then
             -- insert close price to end of Queues and update Sum of closes
 			table.insert(Queues.Indexes, index)
             table.insert(Queues.Values, value)
-	
+
             -- DUBLICATE "and (index <= To)" - if Queues growth up max size
 			--[[ if ((#Queues.Indexes > Size) and (#Queues.Values > Size)) then
                 --remove earlest price from Queues
 				table.remove(Queues.Indexes, 1)
                 table.remove(Queues.Values, 1)
 			end ]]
-		end 
+		end
 		return Queues
 	end
-end 
+end
 
 -- constructor
 q = Queue(11, 33)
+z = Queue(27, 44)
+
+print(tostring(q) .. "\t" .. tostring(z))
 
 for i = 1, 50 do
     qq = q(i, i/2)
+	zz = z(i, i*2)
 end
 
-for i, j in pairs(qq.Indexes) do
-    print( 'Queues.Indexes[' .. i .. "]=" .. j)
-end
-
-for x, y in pairs(qq.Values) do
-    print( 'Queues.Values[' .. x .. "]=" .. y)
-end 
+print(tostring(qq) .. "\t" .. tostring(zz))
 
 for i = 1, 50 do
-    print('qq[' .. i .. "/" .. tostring(qq.Indexes[i]).. "]=" .. tostring(qq.Values[i]))
+    print('[' .. i .. ']: ' .. '\tqq[' .. tostring(qq.Indexes[i]).. "]=" .. tostring(qq.Values[i]) .. '\tzz[' .. tostring(zz.Indexes[i]).. "]=" .. tostring(zz.Values[i]))
 end
 
