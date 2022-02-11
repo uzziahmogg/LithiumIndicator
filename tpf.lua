760cef2f6453665e14f420fe7508f73213403c5d
Uzziah Mogg <39528065+uzziahmogg@users.noreply.github.com>
2022-02-11 15:01:59 +0300

    Add files via upload

--- tpf.lua
+++ tpf.lua
@@ -11,7 +11,7 @@
 --
 -- export string in readable format
 --
-function exportstring( s )
+function exportstring(s)
    return string.format("%q", s)
 end
 
@@ -22,60 +22,71 @@ function table_save(tag, file, tbl)
    file:write(charE .. tag .. "{" .. charE)
 
    for idx, t in ipairs(tables) do
-      file:write("-- Table: {" .. idx .. "}" .. charE)
+      file:write("-- Table: [" .. idx .. "]" .. charE)
       file:write("{" .. charE)
       local thandled = {}
 
-      for i,v in ipairs( t ) do
+      for i, v in ipairs(t) do
          thandled[i] = true
-            local stype = type( v )
-            if stype == "table" then
-               if not lookup[v] then
-                  table.insert( tables, v )
-                  lookup[v] = #tables
-               end
-               file:write( charS.."{"..lookup[v].."},"..charE )
-            elseif stype == "string" then
-               file:write(  charS..exportstring( v )..","..charE )
-            elseif stype == "number" then
-               file:write(  charS..tostring( v )..","..charE )
+
+         local stype = type(v)
+         if (stype == "table") then
+            if (not lookup[v]) then
+               table.insert(tables, v)
+               lookup[v] = #tables
             end
+
+            file:write(charS .. "{" .. lookup[v] .. "}," .. charE)
+
+         elseif (stype == "string") then
+            file:write(charS .. exportstring(v) .."," .. charE)
+
+         elseif (stype == "number") then
+            file:write(charS .. tostring(v) .. "," .. charE)
          end
-
-         for i,v in pairs( t ) do
-            if (not thandled[i]) then
-
-               local str = ""
-               local stype = type( i )
-               if stype == "table" then
-                  if not lookup[i] then
-                     table.insert( tables,i )
-                     lookup[i] = #tables
-                  end
-                  str = charS.."[{"..lookup[i].."}]="
-               elseif stype == "string" then
-                  str = charS.."["..exportstring( i ).."]="
-               elseif stype == "number" then
-                  str = charS.."["..tostring( i ).."]="
-               end
-
-               if str ~= "" then
-                  stype = type( v )
-                  if stype == "table" then
-                     if not lookup[v] then
-                        table.insert( tables,v )
-                        lookup[v] = #tables
-                     end
-                     file:write( str.."{"..lookup[v].."},"..charE )
-                  elseif stype == "string" then
-                     file:write( str..exportstring( v )..","..charE )
-                  elseif stype == "number" then
-                     file:write( str..tostring( v )..","..charE )
-                  end
-               end
-            end
-         end
-         file:write( "},"..charE )
       end
-      file:write( "}"..charE )
+
+      for i, v in pairs(t) do
+         if (not thandled[i]) then
+            local str = ""
+
+            local stype = type(i)
+            if (stype == "table") then
+               if (not lookup[i]) then
+                  table.insert(tables, i)
+                  lookup[i] = #tables
+               end
+
+               str = charS .. "[{" .. lookup[i] .. "}]="
+
+            elseif (stype == "string") then
+               str = charS .. "[" .. exportstring(i) .. "]="
+
+            elseif (stype == "number") then
+               str = charS .. "[" .. tostring(i) .. "]="
+            end
+
+            if (str ~= "") then
+
+               stype = type(v)
+               if (stype == "table") then
+                  if (not lookup[v]) then
+                     table.insert(tables, v)
+                     lookup[v] = #tables
+                  end
+
+                  file:write(str .. "{" .. lookup[v] .. "}," .. charE)
+
+               elseif (stype == "string") then
+                  file:write(str .. exportstring(v) .. "," .. charE)
+
+               elseif (stype == "number") then
+                  file:write(str .. tostring(v) .. "," .. charE)
+               end
+            end
+         end
+      end
+      file:write("}," .. charE)
+   end
+   file:write("}" .. charE)
 end
